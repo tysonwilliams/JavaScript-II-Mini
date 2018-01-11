98b96ee2555a535c6999cb4b9b459aacd27a9987
Tyson Williams <tyson.j.williams@gmail.com>
2018-01-11 16:02:08 -0700

    Refactor to Class

--- psuedoclassical-inheritance.js
+++ psuedoclassical-inheritance.js
@@ -1,32 +1,47 @@
-function Fruit(attrs) {
-  this.type = attrs.type;
-  this.name = attrs.name;
-  this.isRipe = attrs.isRipe;
-  this.calories = attrs.calories;
-}
-
-Fruit.prototype.calculateCalories = function() {
-  console.log(`Calories in a ${this.name} are ${this.calories * 200}`);
-};
-
-function Banana(banAttrs) {
-  Fruit.call(this, banAttrs);
-  this.doMonkeysEat = banAttrs.doMonkeysEat;
-}
-
-function Kiwi(kiwiAttrs) {
-  Fruit.call(this, kiwiAttrs);
-  this.isFuzzy = kiwiAttrs.isFuzzy;
-  this.isPeeled = kiwiAttrs.isPeeled;
-}
-
-Kiwi.prototype = Object.create(Fruit.prototype);
-
-Kiwi.prototype.peel = function() {
-  if(this.isPeeled === false) {
-    this.isPeeled = true;
+class Fruit {
+  constructor(attrs) {
+    this.type = attrs.type;
+    this.name = attrs.name;
+    this.isRipe = attrs.isRipe;
+    this.calories = attrs.calories;
   }
-};
+  calculateCalories() {
+    console.log(`Calories in a ${this.name} are ${this.calories * 200}`);
+  }
+}
+
+class Banana extends Fruit {
+  constructor(banAttrs) {
+    super(banAttrs);
+    this.doMonkeysEat = banAttrs.doMonkeysEat;
+  }
+  ripen() {
+    if(this.isRipe === false) {
+      this.isRipe = true;
+    }
+  }
+}
+
+class Kiwi extends Fruit {
+  constructor(kiwiAttrs) {
+    super(kiwiAttrs);
+    this.isFuzzy = kiwiAttrs.isFuzzy;
+    this.isPeeled = kiwiAttrs.isPeeled;
+  }
+  peel() {
+    if(this.isPeeled === false) {
+      this.isPeeled = true;
+    }
+  }
+}
+
+const newBanana = new Banana({
+  type: "Amazon",
+  name: "Bananayums",
+  isRipe: false,
+  calories: 10,
+  doMonkeysEat: true,
+});
 
 const newKiwi = new Kiwi({
   type: "Tree",
@@ -35,22 +50,6 @@ const newKiwi = new Kiwi({
   calories: 4,
   isFuzzy: true,
   isPeeled: false,
-});
-
-Banana.prototype = Object.create(Fruit.prototype);
-
-Banana.prototype.ripen = function() {
-  if(this.isRipe === false) {
-    this.isRipe = true;
-  }
-};
-
-const newBanana = new Banana({
-  type: "Amazon",
-  name: "Bananayums",
-  isRipe: false,
-  calories: 10,
-  doMonkeysEat: true,
 });
 
 console.log(newBanana);
